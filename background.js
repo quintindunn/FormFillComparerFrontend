717a16a81ae9ceb2926646e1ebb546eb776ddbc3
Quintin Dunn <quintindunn67@gmail.com>
2022-06-07 23:44:42 -0400

    Refined permissions, added icon

--- background.js
+++ background.js
@@ -1,51 +1,5 @@
 const url = "https://recoiloff.com/post";
 
-/* FOR FUTURE USAGE
-// Detect URL Change for parsing mail.
-chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-    if (changeInfo.url) {
-        try {
-                chrome.tabs.sendMessage( tabId, {message: 'URL Change', url: changeInfo.url});
-        } catch (error) {
-            
-        }
-
-    }
-  }
-);
-
-
-// Listen for mail data
-chrome.runtime.onMessage.addListener(function(request, sender) {
-    console.log("Mail");
-
-    // Return for service-worker keep alive.
-    if (JSON.stringify(request) === "\"ping\"") return;
-    console.log("Mail2");
-    const postType = "Mail";
-    data = request.message;
-    chrome.storage.local.get("sub_count", function(sub_count) {
-            if (JSON.stringify(sub_count).toString() === "{\"sub_count\":\"NaN\"}") {
-                        console.log("NaN'd");
-                        chrome.storage.local.set({"sub_count": "0"});
-            } else if (JSON.stringify(sub_count).toString() === "{}") {
-                        chrome.storage.local.set({"sub_count": "0"});
-            }
-        });
-    chrome.storage.local.get("identifier", function(result) {
-    chrome.storage.local.get("sub_count", function(sub_count) {
-        console.log(JSON.stringify(sub_count));
-        console.log(JSON.stringify(result));
-        fetch(url + postType, {
-            method: "POST",
-            headers: {'sub-count': JSON.stringify(sub_count), 'identifier': JSON.stringify(result)},
-            body: data
-        });
-    });
-    });
-});
-*/
 // Handle POST requests for form submissions
 chrome.webRequest.onBeforeRequest.addListener(
     function(details)
@@ -64,18 +18,6 @@ chrome.webRequest.onBeforeRequest.addListener(
                 chrome.storage.local.set({"sub_count": '0'});
             }
         });
-        // REMOVE FOR PRODUCTION
-        const log = false;
-        // #####################
-        // Checks to not send unneeded requests
-        if (details.method === "GET") return;
-        if (JSON.stringify(details).toString().includes(",\"requestBody\":{\"formData\":{\"impressionBatch\":")) {if (log) {console.log("impressionBatch");} return;};
-        if (JSON.stringify(details).toString().includes(",\"requestBody\":{\"formData\":{\"draft_response_action_request\":")) {if (log) {console.log("draft/_response_action_request");} return;};
-        if (JSON.stringify(details).toString().includes("\"requestBody\":{\"formData\":{\"viewresponse\":")) {if (log) {console.log("viewresponse");} return;};
-        if (JSON.stringify(details).toString().includes("\"requestBody\":{\"formData\":{\"count\":")) {if (log) {console.log("count");} return;};
-        if (JSON.stringify(details).toString().includes("\"requestBody\":{\"error\":\"Unknown error.\"}")) {if (log) {console.log("UnknownError");} return;};
-        if (JSON.stringify(details).toString().includes("\"requestBody\":{\"formData\":{\"request\":")) {if (log) {console.log("request");} return;};
-        
         
         if (!(details.url).toString().startsWith("https://docs.google.com/forms") && !(details.url).toString().startsWith("https://forms.gle")) {if (log) {console.log("URL");} return;};
 
--- content.js
+++ content.js
@@ -1,35 +1,3 @@
-/* FOR FUTURE USAGE
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(JSON.stringify(request));
-    // Set identifier if unset for database identification
-    chrome.storage.local.get("identifier", function(result) {
-
-        if (JSON.stringify(result).toString() === "{}") {
-            chrome.storage.local.set({"identifier": Math.random().toString().replace("0.", "")});
-        }
-    });
-    // Checks that is mail related.
-    if (!request.url.includes("https://mail.google.com/mail")) return;
-    if (!request.url.includes("#inbox/")) return;
-    // Wait for email to load.
-    setTimeout(function() {
-        console.log("timeout");
-        // Check that email is from Grodski Grader
-        const graderName = "Grodski Grader";
-        const graderEmail = "grodskib@whbschools.org";
-        // Select Email Body
-        var emailData = document.querySelector("#\\:1 > div > div > div > table > tr > td.Bu.bAn > div.nH.if > div.nH.aHU");
-        var emailInnerText = emailData.innerText;
-        var emailInnerHTML = emailData.innerHTML;
-        console.log("Sending");
-        if (!(emailInnerHTML.includes(graderEmail) && emailInnerText.includes(graderName))) return;
-        chrome.runtime.sendMessage({message: emailInnerHTML});
-    }, 1000);
-
-});
-*/
-
-
 var wakeup = function(){
     setTimeout(function(){
         try {
